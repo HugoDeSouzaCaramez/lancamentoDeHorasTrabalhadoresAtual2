d95018eba6d77c130ef7d91dd05d6dc67e91a073
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-02-25 17:06:32 -0300

    adicionando mascaras ao cpf

--- resources/views/employers/create.blade.php
+++ resources/views/employers/create.blade.php
@@ -43,4 +43,21 @@
             </div>
         </div>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('#cpf').keydown(function(){
+                var cpf = $(this).val();
+                cpf = cpf.replace(/\D/g, '');
+                if (cpf.length > 3 && cpf.length < 7) {
+                    cpf = cpf.substring(0, 3) + '.' + cpf.substring(3);
+                } else if (cpf.length > 6 && cpf.length < 10) {
+                    cpf = cpf.substring(0, 3) + '.' + cpf.substring(3, 6) + '.' + cpf.substring(6);
+                } else if (cpf.length > 9) {
+                    cpf = cpf.substring(0, 3) + '.' + cpf.substring(3, 6) + '.' + cpf.substring(6, 9) + '-' + cpf.substring(9, 11);
+                }
+                $(this).val(cpf);
+            });
+        });
+    </script>
 @endsection
